ea8787086ac7fed729dfd0768390d77158abf034
coderDIVYANSH1705 <divyanshmishraspeaks@gmail.com>
2026-06-13 13:42:25 +0530

    fix

--- gyanamreviews/.next/dev/server/pages/_app.js
+++ gyanamreviews/.next/dev/server/pages/_app.js
@@ -1,4 +1,4 @@
 var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_app.js")
-R.c("server/chunks/ssr/1783_next_app_0zepm5b.js")
-R.m("[project]/Review/gyanamreviews/node_modules/next/app.js [ssr] (ecmascript)")
-module.exports=R.m("[project]/Review/gyanamreviews/node_modules/next/app.js [ssr] (ecmascript)").exports
+R.c("server/chunks/ssr/[root-of-the-server]__1v1_ace._.js")
+R.m("[project]/Review/node_modules/next/app.js [ssr] (ecmascript)")
+module.exports=R.m("[project]/Review/node_modules/next/app.js [ssr] (ecmascript)").exports
---
+++ gyanamreviews/.next/dev/static/chunks/Review_gyanamreviews_pages__app_0du2_q-._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/0oa-_next_dist_compiled_1rlygny._.js","static/chunks/0oa-_next_dist_shared_lib_0w78d-r._.js","static/chunks/0oa-_next_dist_client_11clzg1._.js","static/chunks/0oa-_next_dist_0l3k0li._.js","static/chunks/0oa-_next_app_0g_hieq.js","static/chunks/[next]_entry_page-loader_ts_1zdldv1._.js","static/chunks/0oa-_react-dom_1v1juyz._.js","static/chunks/0oa-_0k4mh8e._.js","static/chunks/[root-of-the-server]__1exzrg5._.js"],
+    source: "entry"
+});
